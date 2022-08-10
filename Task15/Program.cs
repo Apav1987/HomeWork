4dcf1a78f25b664151ef6de2f59badeb68e304ed
Andrey <apav1987@gmail.com>
2022-08-10 21:55:52 +0300

    task15

---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число от 1 до 7 ");
+int a =Convert.ToInt32(Console.ReadLine());
+
+if ((6==a)^(7==a))
+{
+    Console.WriteLine("Выходной день");
+}
+if ((1==a)^(2==a)^(3==a)^(4==a)^(5==a))
+{
+    Console.WriteLine("Рабочий день");
+}
