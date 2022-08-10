9317d19ec81fd0918f43b1da8a5ed1b2139f3e56
Andrey <apav1987@gmail.com>
2022-08-10 21:43:14 +0300

    task10

---
+++ Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трехзначное целое число ");
+int a =Convert.ToInt32(Console.ReadLine());
+
+int thirdDel = a / 10; //убираем третью цифру
+
+Console.WriteLine(thirdDel%10); // вторая цифра трехзначного числа
