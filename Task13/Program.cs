56edc2c2b4bd344ebe75e4ff83004e929ff7c201
Andrey <apav1987@gmail.com>
2022-08-10 22:28:45 +0300

    task13

---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число ");
+string a =(Console.ReadLine());
+
+if (a.Length > 2)
+{
+    Console.WriteLine("Третья цифра " + a[2]);
+}
+else
+{
+    Console.WriteLine("Третий цифры нет");
+}
+
