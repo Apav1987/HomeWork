0049813506793dfa5bb8e9a0860a1ecaf14c5df4
Andrey <apav1987@gmail.com>
2022-08-15 22:34:09 +0300

    task19

---
+++ Task19/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите пятизначное число: ");
+string a = Console.ReadLine();
+
+  if (a[0]==a[4] & a[1]==a[3]){
+    Console.WriteLine($"Ваше число: {a} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {a} - Не палиндром.");
+
